92dae6075f7dff9a6437734a0b926514a4c62cfa
immaemi <nam14785@soongsil.ac.kr>
2025-10-15 12:03:47 +0900

    feat: Handle empty or null input returning zero (1-1)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  calculate(text) {
+    if(text === null || text.trim() === "") {
+      return 0;
+    }
+    return text;
+  }
+
+  async run() {
+
+    Console.readLine('덧셈할 문자열을 입력해 주세요.\n', (inputString) => {
+      try {
+        let result = this.calculate(inputString);
+        Console.print(`결과: ${result}`);
+      } catch (error) {
+        Console.print(`[ERROR] ${error.message}`);
+      }
+    });
+
+  }
 }
 
 export default App;
